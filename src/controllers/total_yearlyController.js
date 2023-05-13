3cd048330008eef12b0ac7711284b1260bfa9354
SchadenKai <kairustecsonpersonal@gmail.com>
2023-05-13 14:41:03 +0800

    cleaned up code

--- src/controllers/total_yearlyController.js
+++ src/controllers/total_yearlyController.js
@@ -1,8 +1,5 @@
 const {
-  onSnapshot,
-  getDocs,
   collection,
-  addDoc,
 } = require("firebase/firestore");
 const db = require("../firebase");
 const { CRUD } = require("../crud");
@@ -10,21 +7,6 @@ const { CRUD } = require("../crud");
 const total_yearlyRef = collection(db, "total_yearly");
 
 exports.total_yearlyController = {
-  // getAll : (req, res) => {
-  //     const data = []
-  //     getDocs(total_yearlyRef)
-  //         .then(snapshot => {
-  //             snapshot.docs.forEach(doc => {
-  //                 data.push({...doc.data(), id : doc.id})
-  //             })
-  //         })
-  //         .then(() => {
-  //             res.send(data)
-  //         })
-  //         .catch(e => {
-  //             res.status(500).send({error : e.message})
-  //         })
-  // },
   getAll: async (req, res) => {
     try {
       const data = await CRUD.readAll(total_yearlyRef);
