a3065a378c30a09eaa4c24c0d46cfa2ebc6d87ba
SchadenKai <kairustecsonpersonal@gmail.com>
2023-05-29 18:06:48 +0800

    fixed overall_weight

--- src/middlewares/calculateTotal.js
+++ src/middlewares/calculateTotal.js
@@ -8,23 +8,16 @@ const wasteRef = collection(db, "waste");
 exports.calculateTotalMiddleware = async (req, res, next) => {
   const docRef = doc(db, "waste", req.params.id);
   const latestDoc = await CRUD.read(docRef);
-  const request_key = Object.keys(req.body);
-  let req_waste_type;
-  let building_name
-  request_key.forEach((key) => {
-    if (key != "overall_weight") {
-      building_name = key;
-      re
-    }
-  });
-  if(latestDoc[0][building_name].weight[req.body[building_name]]) {
+  let total_weight = latestDoc[0].overall_weight
+  const req_keys = Object.keys(req.body);
+  const building_name = req_keys[0]
 
+  // check if the building has data 
+  if (latestDoc[0][building_name].weight.total != 0) {
+    // if it has data, subtract the total weight of the building from the overall weight
+    total_weight -= latestDoc[0][building_name].weight.total
+  }
   
-  
-
-  let totalWeight = req.body[request_key].weight.total;
-  
-
-  req.body.overall_weight = totalWeight + latestDoc[0].overall_weight;
+  req.body.overall_weight = total_weight + req.body[building_name].weight.total;
   next();
 };
